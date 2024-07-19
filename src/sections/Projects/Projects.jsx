d5c6ce18f6a67b6b362a7d1aaab9e270bd86e97c
PratikMitkar <pratikmitkar12@gmail.com>
2024-07-20 02:34:53 +0530

    add: initial files

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from './ProjectsStyles.module.css';
+import voice_assistant from '../../assets/task_1.png';
+import resume_page from '../../assets/task_2.png';
+import BluinoControl from '../../assets/task_3.png';
+import chatbot from '../../assets/task_4.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={voice_assistant}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="voice assistant"
+          p="python based arduino project"
+        />
+        <ProjectCard
+          src={resume_page}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Portfolio"
+          p="react.js based project"
+        />
+        <ProjectCard
+          src={BluinoControl}
+          link="https://play.google.com/store/apps/details?id=com.CodeCraftPratik.sesors&pli=1"
+          h3="BluinoControl"
+          p="java based android app for arduino"
+        />
+        <ProjectCard
+          src={chatbot}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="chatbot"
+          p="python project using numpy and tensorflow"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
